c3d9a77e8cb83542d059b091d61620b6b20e2f71
alexischas@hotmail.com <alexischas@hotmail.com>
2016-09-05 21:16:34 -0300

    initttttt

---
+++ index.php
@@ -0,0 +1,20 @@
+<html>
+	<head>
+
+
+</head>
+
+<div class="CajaInicio animated shake">
+<form action="destino.php" method="post" id="FormIngreso">
+<label> Ingrese los datos de un rectangulo <br></label>
+<label> Altura <br></label>
+
+<input type = "text" name = "nombre" text =Altura>
+<input type="text" name = "numero">
+
+<input type="submit" class="MibotonUTNMenuInicio">
+</form>
+
+</div>
+
+</html>
