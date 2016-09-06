a690c6857d05e0b41c6341114c4dc81d2f2ce0f7
alexischas@hotmail.com <alexischas@hotmail.com>
2016-09-05 22:01:30 -0300

    sa

--- index.php
+++ index.php
@@ -27,8 +27,8 @@ if(isset($_POST['altura']))
 
 ?>
 
-<svg width="400" height="110">
-  <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
+<svg width=$_POST['altura'] height=$_POST['altura']>
+  <rect width=$_POST['altura'] height=$_POST['altura'] style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
 </svg>
 
 <input type="submit" class="MibotonUTNMenuInicio">
