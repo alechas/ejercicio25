c893b706676eb3df39a0631d42c8e0700e24e6ad
alexischas@hotmail.com <alexischas@hotmail.com>
2016-09-05 21:27:36 -0300

    s

--- index.php
+++ index.php
@@ -1,18 +1,24 @@
 <html>
+
 	<head>
+		<link rel="stylesheet" type="text/css" href="estilo.css">
+    </head>
 
-
-</head>
-
-<div class="CajaInicio animated shake">
-<form action="destino.php" method="post" id="FormIngreso">
+<div class="CajaInicio">
+<form action="calcular.php" method="post" id="FormIngreso">
 <label> Ingrese los datos de un rectangulo <br></label>
+
 <label> Altura <br></label>
+<input type = "text" name = "altura" text =Altura>
 
-<input type = "text" name = "nombre" text =Altura>
-<input type="text" name = "numero">
+<label> Longitud <br></label>
+<input type="text" name = "longitud">
 
-<input type="submit" class="MibotonUTNMenuInicio">
+var ctx=c.getContext("2d");
+ctx.rect(20,20,150,100);
+ctx.stroke();
+
+<input type="submit">
 </form>
 
 </div>
