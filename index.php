36832f02cfecfcae8fd80ae81960dea9b3cddd81
alexischas@hotmail.com <alexischas@hotmail.com>
2016-09-05 21:30:50 -0300

    a

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
 
 <label> Longitud <br></label>
 <input type="text" name = "longitud">
-
+<br>
 <svg width="400" height="110">
   <rect width="300" height="100" style="fill:rgb(0,0,255);stroke-width:3;stroke:rgb(0,0,0)" />
 </svg>
