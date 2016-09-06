f868313ffc26d23f3c214d4c596977c517f38023
alexischas@hotmail.com <alexischas@hotmail.com>
2016-09-05 21:28:15 -0300

    s

--- index.php
+++ index.php
@@ -14,9 +14,11 @@
 <label> Longitud <br></label>
 <input type="text" name = "longitud">
 
+<?php
 var ctx=c.getContext("2d");
 ctx.rect(20,20,150,100);
 ctx.stroke();
+?>
 
 <input type="submit">
 </form>
